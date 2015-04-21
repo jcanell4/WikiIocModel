5233edab47b9418c10d28631ea154d3689430e93
jcanell4 <jcanell4@ioc.cat>
2015-04-21 02:26:26 +0200

    adding tplConf method

--- DokuModelAdapter.php
+++ DokuModelAdapter.php
@@ -266,6 +266,7 @@ class DokuModelAdapter implements WikiIocModel {
         if($INFO["exists"]){
             throw new PageAlreadyExistsException($pid,$lang['pageExists']);
         }
+        //
         $this->doSavePreProcess();
         return $this->getFormatedPageResponse();
     }
@@ -509,6 +510,16 @@ class DokuModelAdapter implements WikiIocModel {
             $ret = DOKU_INC . 'lib/tpl/' . $conf['template'] . '/';
         }
         return $ret;
+    }
+    
+    // configuration methods
+    /**
+     * tpl_getConf($id)
+     *
+     * use this function to access template configuration variables
+     */
+    public function tplConf($id){
+        return tpl_getConf($id);
     }
 
     /**
