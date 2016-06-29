cb515d5e75dd7925be1888232db8bdbbf833e672
jcanell4 <jcanell4@xtec.cat>
2016-06-30 01:46:09 +0200

    set of default project classes

---
+++ projects/defaultProject/projectClassCfg.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Ruta de les classes per defecte del projecte defaultProject
+ *
+ * @culpable Rafael Claver
+  */
+if (!defined("DOKU_INC")){
+    die();
+}
+
+class projectClassCfg {
+
+    const DEF = DOKU_INC.'lib/plugins/wikiiocmodel/projects/defaultProject/';
+    static $cfg = array (
+                     "Action" => array (
+                                    projectClassCfg::DEF."actions"
+                                   ,projectClassCfg::DEF."actions/extra"
+                                 )
+                    ,"Authorization" => array (
+                                           projectClassCfg::DEF."authorization"
+                                        )
+                    ,"Model" => array (
+                                   projectClassCfg::DEF."datamodel"
+                                )
+                  );
+
+    public function getDefaultClassDir($name) {
+        return projectClassCfg::$cfg[$name];
+    }
+
+}
