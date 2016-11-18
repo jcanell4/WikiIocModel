b18d3f080e753117f47623c81ddeb79f66169789
rclaver <rclaver@xtec.cat>
2016-11-18 14:57:56 +0100

    estandarización de la definición de constantes

--- projects/documentation/authorization/CreateAuthorization.php
+++ projects/documentation/authorization/CreateAuthorization.php
@@ -6,10 +6,10 @@
  * @author Rafael Claver
  */
 if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_IOC_MODEL')) define('DOKU_IOC_MODEL', DOKU_INC . 'lib/plugins/wikiiocmodel/projects/defaultProject/');
+define('WIKI_IOC_PROJECT', DOKU_INC . "lib/plugins/wikiiocmodel/projects/documentation/");
 
 require_once (DOKU_INC . 'inc/auth.php');
-require_once (DOKU_IOC_MODEL . 'authorization/PageCommandAuthorization.php');
+require_once (WIKI_IOC_PROJECT . 'authorization/PageCommandAuthorization.php');
 
 class CreateAuthorization extends PageCommandAuthorization {
 
--- projects/documentation/authorization/UploadAuthorization.php
+++ projects/documentation/authorization/UploadAuthorization.php
@@ -6,10 +6,10 @@
  * @author Rafael Claver
  */
 if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_IOC_MODEL')) define('DOKU_IOC_MODEL', DOKU_INC . "lib/plugins/wikiiocmodel/projects/defaultProject/");
+define('WIKI_IOC_PROJECT', DOKU_INC . "lib/plugins/wikiiocmodel/projects/documentation/");
 
 require_once (DOKU_INC . 'inc/auth.php');
-require_once (DOKU_IOC_MODEL . 'authorization/CommandAuthorization.php');
+require_once (WIKI_IOC_PROJECT . 'authorization/CommandAuthorization.php');
 
 class UploadAuthorization extends CommandAuthorization {
 
