b60f431071ee2f9ee655005fc9af0b812273d3f9
Xavier García <xaviergaro.dev@gmail.com>
2017-12-04 09:52:44 +0100

    Fixed edit structured without draft

--- projects/defaultProject/actions/RawPartialPageAction.php
+++ projects/defaultProject/actions/RawPartialPageAction.php
@@ -89,8 +89,8 @@ class RawPartialPageAction extends PageAction implements ResourceLockerInterface
         $response = [];
         $data = $this->getModel()->getData(TRUE);
 
-        // 0) Si ja hi ha un chunk en edició, s'han d'ignorar els drafts
-        $ignoreDrafts = count($this->params[PageKeys::KEY_EDITING_CHUNKS])>1;
+        // 0) Si ja hi ha un chunk en edició, s'han d'ignorar els drafts FULL: comprovem si s'ha passat la data de de l'structured
+        $ignoreDrafts = count($this->params[PageKeys::KEY_EDITING_CHUNKS])>1 && !isset($this->params['structured_last_local_draft_time']);
 
         // 1) Ja s'ha recuperat el draft local
         if ($this->params[PageKeys::KEY_RECOVER_LOCAL_DRAFT] && !$ignoreDrafts) {
