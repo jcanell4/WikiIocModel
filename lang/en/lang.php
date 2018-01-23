e43be8e8986856bbb25d7808cb5dc2ac142e9ee2
rclaver <rclaver@xtec.cat>
2018-01-23 11:27:16 +0100

    $lang['reverted']

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,12 @@
 <?php
 /**
  * English language file
- * 
  * @author Joseo Cañellas<jcanell4@ioc.cat>
  */
 
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
 $lang['createDefaultText'] = 'Pàgina creada per defecte';
 $lang['conflictsSaving'] = "Hi ha conflictes amb altres versions. No s'han pogut guardar els canvis";
 $lang['saved'] = "Els canvis s'han guardat correctament";
 $lang['pageNotFound'] = "No s'ha trobat la pàgina %s";
 $lang['pageExists'] = "La pàgina %s ja existeix";
+$lang['reverted'] = "La reversió s'ha completat correctament";
