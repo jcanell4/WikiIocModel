2e99ebbda7c3d9f3cfdd8e0408cd9c30f5ec5204
rclaver <rclaver@xtec.cat>
2018-02-09 10:40:14 +0100

    ADD _saveRevision AND addProjectLogEntry

--- projects/documentation/actions/ProjectMetadataAction.php
+++ projects/documentation/actions/ProjectMetadataAction.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Clase que agrupa los elementos comunes de las Actions de este proyecto
+ * Clase que define los elementos comunes de las Actions de este proyecto
  *
  * @culpable Rafael Claver
  */
@@ -8,16 +8,16 @@ if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once DOKU_PLUGIN . "wikiiocmodel/actions/AbstractWikiAction.php";
 require_once DOKU_PLUGIN . "wikiiocmodel/projects/documentation/datamodel/ProjectModel.php";
-require_once DOKU_PLUGIN . "ajaxcommand/defkeys/ProjectKeys.php";
 
 abstract class ProjectMetadataAction extends AbstractWikiAction {
 
     protected $persistenceEngine;
     protected $projectModel;
 
-    public function __construct($persistenceEngine) {
-        $this->persistenceEngine = $persistenceEngine;
-        $this->projectModel = new ProjectModel($persistenceEngine);
+    public function init($modelManager) {
+        parent::init($modelManager);
+        $this->persistenceEngine = $modelManager->getPersistenceEngine();
+        $this->projectModel = new ProjectModel($this->persistenceEngine);
     }
 
     protected function idToRequestId($requestId) {
