c63ec95ef7be5955949e7de6dff7f6a9e6855337
jcanell4 <jcanell4@xtec.cat>
2018-02-21 00:34:16 +0100

    Clean code

--- projects/documentation/actions/ProjectMetadataAction.php
+++ projects/documentation/actions/ProjectMetadataAction.php
@@ -20,7 +20,6 @@ abstract class ProjectMetadataAction extends AbstractWikiAction {
         $this->projectModel = new ProjectModel($this->persistenceEngine);
     }
 
-
     protected function idToRequestId($requestId) {
         $id = str_replace(":", "_", $requestId);
         return $id;
