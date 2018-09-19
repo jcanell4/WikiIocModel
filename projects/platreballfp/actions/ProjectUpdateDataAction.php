161dd74e6e267a8f6f2edbe292aa670b1ff3992d
rclaver <rclaver@xtec.cat>
2018-09-19 09:25:45 +0200

    command_plugin_wikiiocmodel_projects_platreballfp_projectUpdate: comando asociado al botón "updateProject" ProjectUpdateResponseHandler: response asociada al comando updateProject (muestra los cambios) ProjectUpdateDataAction: Ejecuta las funciones de actualización mediante los valores descritos en la configuración del tipo de proyecto

---
+++ projects/platreballfp/actions/ProjectUpdateDataAction.php
@@ -0,0 +1,73 @@
+<?php
+if (!defined('DOKU_INC')) die();
+if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_INC."lib/plugins/wikiiocmodel/");
+include_once WIKI_IOC_MODEL."projects/platreballfp/actions/ViewProjectMetaDataAction.php";
+
+class ProjectUpdateDataAction extends ViewProjectMetaDataAction {
+
+    protected function runAction() {
+        $projectType = $this->params[ProjectKeys::KEY_PROJECT_TYPE];
+
+        $projectModel = $this->getModel();
+        $response = $projectModel->getDataProject();
+
+        $confProjectType = $this->modelManager->getConfigProjectType();
+        //obtenir la ruta de la configuració per a aquest tipus de projecte
+        $projectTypeConfigFile = $projectModel->getProjectTypeConfigFile($projectType);
+
+        $cfgProjectModel = $confProjectType."ProjectModel";
+        $configProjectModel = new $cfgProjectModel($this->persistenceEngine);
+        $configProjectModel->init($projectTypeConfigFile, $confProjectType);
+
+        //Obtenir les dades de la configuració d'aquest tipus de projecte
+        $projectFileName = $projectModel->getProjectFileName();
+        $metaDataSubset = ($this->params[ProjectKeys::KEY_METADATA_SUBSET]) ? $this->params[ProjectKeys::KEY_METADATA_SUBSET] : ProjectKeys::VAL_DEFAULTSUBSET;
+        $metaDataConfigProject = $configProjectModel->getMetaDataProject($projectFileName, $metaDataSubset);
+
+        if ($metaDataConfigProject['arraytaula']) {
+            $arraytaula = json_decode($metaDataConfigProject['arraytaula'], TRUE);
+            $processArray = array();
+
+            foreach ($arraytaula as $elem) {
+
+                switch ($elem['type']) {
+                    case "fieldSubstitution":
+                        $processor = "FieldSingleSubstitutionProjectUpdateProcessor";
+                        if ( !isset($processArray[$processor]) ) {
+                            $processArray[$processor] = new $processor;
+                        }
+                        $processArray[$processor]->runProcess($elem['value'], $elem['parameters'], $response);
+                        break;
+
+                    case "fieldIncrement":
+                        $processor = "FieldIncrementProjectUpdateProcessor";
+                        if ( !isset($processArray[$processor]) ) {
+                            $processArray[$processor] = new $processor;
+                        }
+                        $processArray[$processor]->runProcess($elem['value'], $elem['parameters'], $response);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+
+            if ($elem) {
+                $metaData = [
+                    ProjectKeys::KEY_ID_RESOURCE => $this->params[ProjectKeys::KEY_ID],
+                    ProjectKeys::KEY_METADATA_SUBSET => ProjectKeys::VAL_DEFAULTSUBSET,
+                    ProjectKeys::KEY_PERSISTENCE => $this->persistenceEngine,
+                    ProjectKeys::KEY_PROJECT_TYPE => $projectType,
+                    ProjectKeys::KEY_METADATA_VALUE => json_encode($response)
+                ];
+                $projectModel->setData($metaData);    //actualiza el contenido en 'mdprojects/'
+
+                $response = parent::runAction();
+            }
+        }
+
+        return $response;
+    }
+
+}
