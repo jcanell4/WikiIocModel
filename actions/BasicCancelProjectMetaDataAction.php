6ad1ac9c0ce35271954ac62570cdea01c24e7d0a
rclaver <rclaver@xtec.cat>
2018-09-19 09:28:33 +0200

    Extracción de sharedRunAction para ser utilizada por clases hija

--- actions/BasicCancelProjectMetaDataAction.php
+++ actions/BasicCancelProjectMetaDataAction.php
@@ -4,31 +4,29 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
 
 class BasicCancelProjectMetaDataAction extends BasicViewProjectMetaDataAction implements ResourceUnlockerInterface {
 
-    private $resourceLocker;
+    protected function runAction() {
+        $response = self::sharedRunAction($this);
 
-    public function init($modelManager) {
-        parent::init($modelManager);
-        $this->resourceLocker = new ResourceLocker($this->persistenceEngine);
+        if ($this->params[ProjectKeys::KEY_NO_RESPONSE]) {
+            $response[ProjectKeys::KEY_CODETYPE] = 0;
+        }else {
+            $response = parent::runAction();
+        }
+
+        return $response;
     }
 
-    protected function runAction() {
-        $lockStruct = $this->leaveResource(TRUE);
+    public static function sharedRunAction($self) {
+        $lockStruct = $self->leaveResource(TRUE);
         if ($lockStruct['state']) {
             $response['lockInfo'] = $lockStruct['info']['locker'];
             $response['lockInfo']['state'] = $lockStruct['state'];
         }
-        //if (isset($this->params[ProjectKeys::KEY_LEAVERESOURCE])) {  }
 
-        if (!$this->params[ProjectKeys::KEY_KEEP_DRAFT]) {
-            $this->getModel()->removeDraft();
+        if (!$self->params[ProjectKeys::KEY_KEEP_DRAFT]) {
+            $self->getModel()->removeDraft();
         }
 
-        if ($this->params[ProjectKeys::KEY_NO_RESPONSE] ) {
-            $response[ProjectKeys::KEY_CODETYPE] = 0;
-            return $response;
-        }
-
-        $response = parent::runAction();
         return $response;
     }
 
@@ -37,20 +35,6 @@ class BasicCancelProjectMetaDataAction extends BasicViewProjectMetaDataAction im
             $new_message = $this->generateInfo("info", WikiIocLangManager::getLang('project_canceled'), $this->params[ProjectKeys::KEY_ID]);
             $response['info'] = $this->addInfoToInfo($response['info'], $new_message);
         }
-    }
-
-   /**
-     * Mmètode que s'ha d'executar en iniciar el desbloqueig o també quan l'usuari cancel·la la demanda
-     * de bloqueig. Per defecte no es desbloqueja el recurs, perquè actualment el desbloqueig es realitza internament
-     * a les funcions natives de la wiki. Es fa el desbloqueig directament aquí, si es passa el paràmetre amb valor TRUE.
-     * EL mètode retorna una constant amb el resultat obtingut de la petició.
-     *
-     * @param bool $unlock
-     * @return int
-     */
-    public function leaveResource($unlock = FALSE) {
-        $this->resourceLocker->init($this->params);
-        return $this->resourceLocker->leaveResource($unlock);
     }
 
 }
