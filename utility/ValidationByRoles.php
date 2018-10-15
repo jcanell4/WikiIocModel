5efb2998e1d717a47d71019612f8cfa0fddf311f
rclaver <rclaver@xtec.cat>
2018-10-15 17:24:29 +0200

    error función getRole()

--- utility/ValidationByRoles.php
+++ utility/ValidationByRoles.php
@@ -1,26 +1,24 @@
 <?php
-
 if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 
 require_once(DOKU_PLUGIN . "wikiiocmodel/utility/ValidateWithPermission.php");
 
-class ValidationByRoles extends ValidateWithPermission
-{
+class ValidationByRoles extends ValidateWithPermission {
 
     function validate($data)
     {
-        $role = $this->permission->getRole();
+        $role = $this->permission->getRol();
         $ret = TRUE;
 
         if(is_array($role)){
             for ($i=0; $i<count($role) && $ret; $i++){
-                $ret = !in_array($role[$i], $data);                
+                $ret = !in_array($role[$i], $data);
             }
         }else if(strpos($role, ',') !== false){
             $arole = explode(',', $role);
             for ($i=0; $i<count($arole) && $ret; $i++){
-                $ret = !in_array($arole[$i], $data);                
+                $ret = !in_array($arole[$i], $data);
             }
         }else{
             $ret = !in_array($role, $data);
