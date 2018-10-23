99242799b1a9a92561da24b082c7fb8ab4e73828
rclaver <rclaver@xtec.cat>
2018-10-23 14:02:13 +0200

    $projectModel->init( array )

--- actions/CreateSubprojectMetaDataAction.php
+++ actions/CreateSubprojectMetaDataAction.php
@@ -17,14 +17,13 @@ class CreateSubprojectMetaDataAction extends ProjectMetadataAction {
         $new_projectType = $this->params[ProjectKeys::KEY_PROJECT_TYPE];
 
         $projectModel = $this->getModel();
-        //$projectModel->init($parent_id, $parent_projectType);
         $projectModel->init([ProjectKeys::KEY_ID              => $parent_id,
                              ProjectKeys::KEY_PROJECT_TYPE    => $parent_projectType,
                              ProjectKeys::KEY_METADATA_SUBSET => $this->params[ProjectKeys::KEY_METADATA_SUBSET],
                              ProjectKeys::KEY_PROJECTTYPE_DIR => $this->params[ProjectKeys::KEY_PROJECTTYPE_DIR]
                            ]);
 
-        //Verifica que el proyecto solicitado sea un proyecto permitido
+        //Verifica que el proyecto solicitado sea un proyecto existente
         $listProjectTypes = $projectModel->getListProjectTypes($parent_projectType, $this->params[ProjectKeys::KEY_METADATA_SUBSET], $this->params[ProjectKeys::KEY_PROJECTTYPE_DIR]);
         if (!in_array($new_projectType, $listProjectTypes)) {
             throw new UnknownProjectException($new_id, "El tipus de projecte so·licitat no està permés.");
