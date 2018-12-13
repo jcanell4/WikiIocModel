2a722f459f6753b2f1ae159486a026989c143a93
rclaver <rclaver@xtec.cat>
2018-12-13 14:51:07 +0100

    simplificación y eliminación de JSON.php

--- metadata/MetaDataEntityFactory.php
+++ metadata/MetaDataEntityFactory.php
@@ -6,31 +6,29 @@
 if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . "wikiiocmodel/");
-
-require_once (DOKU_INC . "inc/JSON.php");
 require_once (WIKI_IOC_MODEL . "metadata/MetaDataExceptions.php");
-require_once (WIKI_IOC_MODEL . "metadata/metadataconfig/MetaDataDaoConfig.php");
 
 class MetaDataEntityFactory {
     /**
-     * Call DaoConfig to obtain MetaDataEntity ns and return an object from this
+     * obtain MetaDataEntity ns and return an object from this
      * @return MetaDataEntity object (from ns returned by MetaDataDaoConfig)
      */
     public static function getObject($projectType, $metaDataSubSet, $persistence) {
-
-        $classesNameSpaces = MetaDataDaoConfig::getMetaDataConfig($projectType, $metaDataSubSet, $persistence);
-        $objClassesNameSpaces = json_decode($classesNameSpaces);
+        $projectMetaDataQuery = $persistence->createProjectMetaDataQuery(FALSE, $metaDataSubSet, $projectType);
+        $classesNameSpaces = $projectMetaDataQuery->getMetaDataConfig(ProjectKeys::KEY_METADATA_CLASSES_NAMESPACES);
+        $objClassesNameSpaces = json_decode($classesNameSpaces)->$metaDataSubSet;
         if (!isset($objClassesNameSpaces->MetaDataEntity) || $objClassesNameSpaces->MetaDataEntity == NULL) {
             throw new ClassEntityNotFound();
         }
-        $projectTypeDir = $persistence->createProjectMetaDataQuery(FALSE, $metaDataSubSet, $projectType)->getProjectTypeDir();
+        $nameMetaDataEntity = $objClassesNameSpaces->MetaDataEntity;
+        $projectTypeDir = $projectMetaDataQuery->getProjectTypeDir();
         $dir = implode("/", explode("/", $projectTypeDir, -2));
-        $metadataPath = "$dir/" . $objClassesNameSpaces->MetaDataEntity . "/metadata/MetaDataEntity.php";
+        $metadataPath = "$dir/$nameMetaDataEntity/metadata/MetaDataEntity.php";
         if (! file_exists($metadataPath))
-            $metadataPath = WIKI_IOC_MODEL . "projects/" . $objClassesNameSpaces->MetaDataEntity . "/metadata/MetaDataEntity.php";
+            $metadataPath = WIKI_IOC_MODEL . "projects/$nameMetaDataEntity/metadata/MetaDataEntity.php";
         require_once ($metadataPath);
 
-        $fully_qualified_name = $objClassesNameSpaces->MetaDataEntity . "\\MetaDataEntity";
+        $fully_qualified_name = "$nameMetaDataEntity\\MetaDataEntity";
 
         return new $fully_qualified_name(MetaDataDaoConfig::getMetaDataStructure($projectType, $metaDataSubSet, $persistence),
                                          MetaDataDaoConfig::getMetaDataTypesDefinition($projectType, $metaDataSubSet, $persistence));
--- metadata/MetaDataRenderFactory.php
+++ metadata/MetaDataRenderFactory.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Component: Project / MetaData
- * Status: @@Development
  * Purposes:
  * - Render objects supplier
  * @author Miguel Àngel Lozano Márquez<mlozan54@ioc.cat>
@@ -9,35 +8,32 @@
 if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . "wikiiocmodel/");
-
-require_once( DOKU_INC . "inc/JSON.php");
 require_once (WIKI_IOC_MODEL . "metadata/MetaDataExceptions.php");
-require_once (WIKI_IOC_MODEL . "metadata/metadataconfig/MetaDataDaoConfig.php");
 
 class MetaDataRenderFactory {
     /**
      * Purpose:
-     * - Call DaoConfig to obtain MetaDataRender ns and return an object from this
+     * - obtain MetaDataRender ns and return an object from this
      * Restrictions:
      * - mandatory all parameters
-     * - DaoConfig returns null, Exception ClassRenderNotFound 5080
      * @return MetaDataRender object (from ns returned by MetaDataDaoConfig)
      */
     public static function getObject($projectType, $metaDataSubSet, $persistence) {
-
-        $jSONArray = MetaDataDaoConfig::getMetaDataConfig($projectType, $metaDataSubSet, $persistence);
-        $arrayConfigPre = json_decode($jSONArray);
-        if (!isset($arrayConfigPre->MetaDataRender) || $arrayConfigPre->MetaDataRender == '' || $arrayConfigPre->MetaDataRender == null) {
+        $projectMetaDataQuery = $persistence->createProjectMetaDataQuery(FALSE, $metaDataSubSet, $projectType);
+        $classesNameSpaces = $projectMetaDataQuery->getMetaDataConfig(ProjectKeys::KEY_METADATA_CLASSES_NAMESPACES);
+        $objClassesNameSpaces = json_decode($classesNameSpaces)->$metaDataSubSet;
+        if (!isset($objClassesNameSpaces->MetaDataRender) || $objClassesNameSpaces->MetaDataRender == NULL) {
             throw new ClassRenderNotFound();
         }
-        $projectTypeDir = $persistence->createProjectMetaDataQuery(FALSE, $metaDataSubSet, $projectType)->getProjectTypeDir();
+        $nameMetaDataRender = $objClassesNameSpaces->MetaDataRender;
+        $projectTypeDir = $projectMetaDataQuery->getProjectTypeDir();
         $dir = implode("/", explode("/", $projectTypeDir, -2));
-        $metadataPath = "$dir/" . $arrayConfigPre->MetaDataRender . "/metadata/MetaDataRender.php";
+        $metadataPath = "$dir/$nameMetaDataRender/metadata/MetaDataRender.php";
         if (! file_exists($metadataPath))
-            $metadataPath = WIKI_IOC_MODEL . "projects/" . $arrayConfigPre->MetaDataRender . "/metadata/MetaDataRender.php";
+            $metadataPath = WIKI_IOC_MODEL . "projects/$nameMetaDataRender/metadata/MetaDataRender.php";
         require_once ($metadataPath);
 
-        $fully_qualified_name = $arrayConfigPre->MetaDataRender . '\\' . "MetaDataRender";
+        $fully_qualified_name = "$nameMetaDataRender\\MetaDataRender";
         return new $fully_qualified_name();
     }
 
