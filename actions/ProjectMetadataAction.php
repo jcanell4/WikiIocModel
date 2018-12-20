7b5115601a140072b2de400ba292903d94bee8ca
rclaver <rclaver@xtec.cat>
2018-12-20 15:04:03 +0100

    KEY_PROJECT_EXTRADATA

--- actions/ProjectMetadataAction.php
+++ actions/ProjectMetadataAction.php
@@ -38,7 +38,7 @@ abstract class ProjectMetadataAction extends AbstractWikiAction {
     protected function postResponseProcess(&$response) {
         if ($this->params[ProjectKeys::KEY_METADATA_SUBSET]!=="undefined" && $this->params[ProjectKeys::KEY_METADATA_SUBSET] !== ProjectKeys::VAL_DEFAULTSUBSET) {
             //$response[ProjectKeys::KEY_ID] = $this->projectModel->addSubSetSufix($response[ProjectKeys::KEY_ID], $this->params[ProjectKeys::KEY_METADATA_SUBSET]);
-            $response['projectExtraData'][ProjectKeys::KEY_METADATA_SUBSET] = $this->params[ProjectKeys::KEY_METADATA_SUBSET];
+            $response[ProjectKeys::KEY_PROJECT_EXTRADATA][ProjectKeys::KEY_METADATA_SUBSET] = $this->params[ProjectKeys::KEY_METADATA_SUBSET];
             $response['isSubSet'] = TRUE;
         }
     }
