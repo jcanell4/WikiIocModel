878548e7f33338de40745d54aae8470e8641b36c
professor <professor@debian8>
2019-03-13 13:55:58 +0100

    correcció error

--- actions/UserListAction.php
+++ actions/UserListAction.php
@@ -7,6 +7,7 @@ if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . "wikiiocmodel/");
 require_once WIKI_IOC_MODEL . "authorization/PagePermissionManager.php";
+require_once WIKI_IOC_MODEL . "actions/AbstractWikiAction.php";
 
 class UserListAction  extends AbstractWikiAction {
 
