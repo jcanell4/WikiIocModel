dd933c9fa009c632006128cae6d96d0fa132e5c7
rclaver <rclaver@xtec.cat>
2019-05-20 10:15:21 +0200

    más bonito

--- projects/guiesges/datamodel/guiesgesProjectModel.php
+++ projects/guiesges/datamodel/guiesgesProjectModel.php
@@ -72,12 +72,12 @@ class guiesgesProjectModel extends AbstractProjectModel {
         $pdir = $this->getProjectMetaDataQuery()->getProjectTypeDir()."metadata/plantilles/";
         $scdir = scandir($pdir);
         foreach($scdir as $file){
-            if($file !== '.' && $file !== '..' && substr($file, -4)===".txt") {
+            if ($file !== '.' && $file !== '..' && substr($file, -4)===".txt") {
                 $plantilla = file_get_contents($pdir.$file);
                 $name = substr($file, 0, -4);
                 $this->dokuPageModel->setData([PageKeys::KEY_ID => $this->id.":".$name,
-                                       PageKeys::KEY_WIKITEXT => $plantilla,
-                                       PageKeys::KEY_SUM => "generate project"]);
+                                               PageKeys::KEY_WIKITEXT => $plantilla,
+                                               PageKeys::KEY_SUM => "generate project"]);
             }
         }
     }
