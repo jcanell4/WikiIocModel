ab64b3b26e133ca59328880d2d0e3bd18deaf702
Xavier García <xaviergaro.dev@gmail.com>
2019-06-25 09:30:54 +0200

    Fixed ftpSendButton visibility. Moved some methods to parent class

--- projects/ptfct/datamodel/ptfctProjectModel.php
+++ projects/ptfct/datamodel/ptfctProjectModel.php
@@ -136,36 +136,4 @@ class ptfctProjectModel extends AbstractProjectModel {
         }
     }
 
-    /**
-     * Averigua si hay fichero para enviar por FTP
-     * @return boolean
-     */
-    public function haveFilesToExportList() {
-        $ret = $this->filesToExportList();
-        return (!empty($ret));
-    }
-
-    /**
-     * Obtiene la lista de ficheros, y sus propiedades, (del configMain.json) que hay que enviar por FTP
-     * @return array
-     */
-    public function filesToExportList() {
-        $ret = array();
-        $metadata = $this->getProjectMetaDataQuery()->getMetaDataFtpSender();
-        foreach ($metadata as $n => $ofile) {
-            $path = ($ofile['local']==='mediadir') ? WikiGlobalConfig::getConf('mediadir')."/". str_replace(':', '/', $this->id)."/" : $ofile['local'];
-            if (($dir = @opendir($path))) {
-                while ($file = readdir($dir)) {
-                    if (!is_dir("$path/$file") && end(explode(".",$file))===$ofile['type']) {
-                        $ret[$n]['file'] = $file;
-                        $ret[$n]['local'] = $path;
-                        $ret[$n]['action'] = $ofile['action'];
-//                        $ret[$n]['remoteBase'] = $ofile['remoteBase'];
-                        $ret[$n]['remoteDir'] = $ofile['remoteDir'];
-                    }
-                }
-            }
-        }
-        return $ret;
-    }
 }
