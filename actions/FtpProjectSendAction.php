df1be485e0c8f7119b1e70ecdfd7cb81703d4bc2
Xavier García <xaviergaro.dev@gmail.com>
2019-06-26 09:49:29 +0200

    Minor Fix

---
+++ actions/FtpProjectSendAction.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of BasicFtpSendAction
+ */
+if (!defined("DOKU_INC")) die();
+require_once DOKU_INC . "lib/lib_ioc/wikiiocmodel/FtpSender.php";
+
+class FtpProjectSendAction extends BasicFtpProjectSendAction{
+
+}
