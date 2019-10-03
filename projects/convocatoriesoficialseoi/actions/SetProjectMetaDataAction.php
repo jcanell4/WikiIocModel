ea9db9c3c156df13629595f92404e33b59a4ecc0
rclaver <rclaver@xtec.cat>
2019-10-03 14:45:21 +0200

    nuevo código en el Abstract : modifyACLPageAndShortcutToPerson

--- projects/convocatoriesoficialseoi/actions/SetProjectMetaDataAction.php
+++ projects/convocatoriesoficialseoi/actions/SetProjectMetaDataAction.php
@@ -5,21 +5,18 @@ class SetProjectMetaDataAction extends BasicSetProjectMetaDataAction
 {
     protected function responseProcess()
     {
-
         $response = parent::responseProcess();
         $model = $this->getModel();
 
         if ($model->isProjectGenerated()) {
             $id = $this->getModel()->getContentDocumentId($response);
             p_set_metadata($id, array('metadataProjectChanged' => time()));
+        }else {
+            $params = $model->buildParamsToPersons($response['projectMetaData'], $response['old_persons']);
+            $model->modifyACLPageAndShortcutToPerson($params);
         }
 
-
-        //$modelAttrib = $model->getModelAttributes();
-
-        $model->generateProject();
-
-
+        $response[ProjectKeys::KEY_GENERATED] = $model->generateProject();
 
         return $response;
     }
--- projects/guiesges/datamodel/guiesgesProjectModel.php
+++ projects/guiesges/datamodel/guiesgesProjectModel.php
@@ -6,7 +6,6 @@
 if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . 'wikiiocmodel/');
-require_once (WIKI_IOC_MODEL . "authorization/PagePermissionManager.php");
 require_once (WIKI_IOC_MODEL . "datamodel/AbstractProjectModel.php");
 
 class guiesgesProjectModel extends AbstractProjectModel {
@@ -23,48 +22,12 @@ class guiesgesProjectModel extends AbstractProjectModel {
         $ret[ProjectKeys::KEY_GENERATED] = $this->projectMetaDataQuery->setProjectGenerated();
 
         if ($ret[ProjectKeys::KEY_GENERATED]) {
-            try {
-                $aAutors = preg_split("/[\s,]+/", $ret['projectMetaData']["autor"]['value']);
-                foreach ($aAutors as $autor) {
-                    //3a. Otorga, a cada Autor, permisos sobre el directorio de proyecto
-                    PagePermissionManager::updatePagePermission($this->id.":*", $autor, AUTH_UPLOAD);
-
-                    //4a. Otorga permisos a cada Autor sobre su propio directorio (en el caso de que no los tenga)
-                    $ns = WikiGlobalConfig::getConf('userpage_ns','wikiiocmodel').$autor.":";
-                    PagePermissionManager::updatePagePermission($ns."*", $autor, AUTH_DELETE, TRUE);
-                    //4b. Incluye la página del proyecto en el archivo de atajos del Autor
-                    $params = [
-                         'id' => $this->id
-                        ,'link_page' => $this->id
-                        ,'user_shortcut' => $ns.WikiGlobalConfig::getConf('shortcut_page_name','wikiiocmodel')
-                    ];
-                    $this->includePageProjectToUserShortcut($params);
-                }
-
-                //3b. Otorga, a los Responsables, permisos sobre el directorio de proyecto
-                $aResponsables = preg_split("/[\s,]+/", $ret['projectMetaData']["responsable"]['value']);
-                foreach ($aResponsables as $responsable) {
-                    if (! in_array($responsable, $aAutors)) {
-                        PagePermissionManager::updatePagePermission($this->id.":*", $responsable, AUTH_UPLOAD);
-                    }
-                }
-            }
-            catch (Exception $e) {
-                $ret[ProjectKeys::KEY_GENERATED] = FALSE;
-                $this->projectMetaDataQuery->setProjectSystemStateAttr("generated", FALSE);
-            }
+            //3. Otorga, a cada 'person', permisos adecuados sobre el directorio de proyecto y añade shortcut
+            $params = $this->buildParamsToPersons($ret['projectMetaData'], NULL);
+            $this->modifyACLPageAndShortcutToPerson($params);
         }
 
         return $ret;
-    }
-
-    /**
-     * Modifica los permisos en el fichero de ACL y la página de atajos del autor
-     * cuando se modifica el autor o el responsable del proyecto
-     * @param array $parArr ['id','link_page','old_autor','old_responsable','new_autor','new_responsable','userpage_ns','shortcut_name']
-     */
-    public function modifyACLPageToUser($parArr) {
-        parent::modifyACLPageToUser($parArr);
     }
 
     public function createTemplateDocument($data){
