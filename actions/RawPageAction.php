193c167bdf2dcd3aec2c7473bd6246de522f374f
Xavier García <xaviergaro.dev@gmail.com>
2019-10-10 13:00:05 +0200

    Added DW2Html (paragraphs, hr, br and headers)

--- actions/RawPageAction.php
+++ actions/RawPageAction.php
@@ -217,16 +217,18 @@ class RawPageAction extends EditPageAction {
     }
 
     protected function translateToDW($text){
-        $trans = new MarkDown2DikuWikiTranslator();
+//        $trans = new MarkDown2DikuWikiTranslator();
 
         $noutext = Html2DWParser::parse($text);
+
+        return $noutext;
 
 //        exec("cd ".DOKU_INC."../pandoc; ./convHtml2MdwFromText.sh \"$text\"", $return, $exit);
 //        if($exit!==0){
 //            throw new SystemExecutionFailedException();
 //        }
 //        $text = implode ( "\n" , $return );
-        return $trans->getRenderedContent($trans->getInstructions($noutext));
+//        return $trans->getRenderedContent($trans->getInstructions($noutext));
     }
 
     protected function translateToHTML($text){
