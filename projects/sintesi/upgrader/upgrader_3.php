347ac7689b1d6639b279477677f97ea8a8b849a0
jcanell4 <jcanell4@xtec.cat>
2020-02-11 14:32:10 +0100

    Nova versió del pla de síntesi

--- projects/sintesi/upgrader/upgrader_3.php
+++ projects/sintesi/upgrader/upgrader_3.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * upgrader_2: Transforma el archivo continguts.txt de los proyectos 'ptfploe'
- *             desde la versión 1 a la versión 2
- * @author rafael
+ * upgrader_3: Transforma el archivo continguts.txt de los proyectos 'ptfploe'
+ *             desde la versión 2 a la versión 3
+ * @author josep
  */
 if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_LIB_IOC')) define('DOKU_LIB_IOC', DOKU_INC."lib/lib_ioc/");
 require_once DOKU_LIB_IOC . "upgrader/CommonUpgrader.php";
 
-class upgrader_2 extends CommonUpgrader {
+class upgrader_3 extends CommonUpgrader {
 
     protected $model;
     protected $metaDataSubSet;
@@ -21,17 +21,6 @@ class upgrader_2 extends CommonUpgrader {
     public function process($type, $filename=NULL) {
         switch ($type) {
             case "fields":
-                $dataProject = $this->model->getMetaDataProject($this->metaDataSubSet);
-                $matches=array();
-                preg_match("/([MC]\d{2})? *-? *(.+)/", $dataProject["modul"], $matches); 
-                if(!empty($matches[1])){
-                    $dataProject['modulId']=$matches[1];
-                }
-                $dataProject['modul'] = $matches[2];
-
-                $this->model->setDataProject(json_encode($dataProject), "Upgrade: version 1 to 2 (canvis en els camps)");
-
-                $ret = TRUE;
                 break;
             case "templates":
                 if ($filename===NULL) { 
@@ -40,13 +29,13 @@ class upgrader_2 extends CommonUpgrader {
                 $doc = $this->model->getRawProjectDocument($filename)."\n";
 
                 $aTokRep = [
-                            [" períodes, de  les ",
-                             " {##nomPeriodePlur##}, de  les "]
+                            ["condition=\"\{#_ARRAY_LENGTH\(\{##dadesAC##\}\)_#\}",
+                             "condition=\"{#_ARRAY_LENGTH({##dadesCompetencies##})_#}"]
                            ];
                 $doc = $this->updateTemplateByReplace($doc, $aTokRep);
 
                 if (!empty($doc)) {
-                    $this->model->setRawProjectDocument($filename, $doc, "Upgrade version 1 to 2");
+                    $this->model->setRawProjectDocument($filename, $doc, "Upgrade version 2 to 3");
                 }
                 $ret = !empty($doc);
         }
