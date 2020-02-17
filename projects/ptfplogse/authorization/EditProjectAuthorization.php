1864515a65d79b20021b88156ec9180558a3ae4c
rclaver <rclaver@xtec.cat>
2020-02-17 17:30:32 +0100

    resoldre conflicte SUPERVISOR

--- projects/ptfplogse/authorization/EditProjectAuthorization.php
+++ projects/ptfplogse/authorization/EditProjectAuthorization.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * EditProjectAuthorization: Extensión clase Autorización para los comandos
- * que precisan una autorización mínima de AUTH_EDIT y que el usuario sea el Responsable o del grupo "admin" o "projectmanager"
+ * que precisan una autorización mínima de AUTH_EDIT y
+ * que el usuario sea el Responsable o del grupo "admin" o "projectmanager"
  * @author Rafael Claver
  */
 if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . "wikiiocmodel/");
-require_once (WIKI_IOC_MODEL . "authorization/ProjectCommandAuthorization.php");
+require_once (WIKI_IOC_MODEL . "authorization/SupervisorProjectAuthorization.php");
 
-class EditProjectAuthorization extends ProjectCommandAuthorization {
+class EditProjectAuthorization extends SupervisorProjectAuthorization {
 
     public function canRun() {
         if (parent::canRun()) {
-            if(($this->permission->getInfoPerm() < AUTH_EDIT || !$this->isUserGroup(array("admin")))
+            if (($this->permission->getInfoPerm() < AUTH_EDIT || !$this->isUserGroup(array("admin")))
                     && !$this->isResponsable() && !$this->isAuthor()) {
                 $this->errorAuth['error'] = TRUE;
                 $this->errorAuth['exception'] = 'InsufficientPermissionToEditProjectException';
