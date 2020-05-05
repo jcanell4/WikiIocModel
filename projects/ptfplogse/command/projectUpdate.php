d0cdfdb956d50d7de06d1c610336d3b241d8eedb
rclaver <rclaver@xtec.cat>
2020-05-05 12:00:17 +0200

    control d'aparició dels botons que li pertoquen

--- projects/ptfplogse/command/projectUpdate.php
+++ projects/ptfplogse/command/projectUpdate.php
@@ -22,7 +22,9 @@ class command_plugin_wikiiocmodel_projects_ptfplogse_projectUpdate extends abstr
                   );
         $action = $this->getModelManager()->getActionInstance("ProjectUpdateDataAction");
         $projectMetaData = $action->get($params);
-        if (!$projectMetaData) throw new UnknownProjectException();
+        if (!$projectMetaData)
+            throw new UnknownProjectException();
+        $this->_addExtraData($projectMetaData);
         return $projectMetaData;
     }
 
