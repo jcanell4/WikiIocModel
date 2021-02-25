e41b57461dbd94d77c6886dc00ba7b7db7a82a27
rclaver <rclaver@xtec.cat>
2021-02-25 14:37:36 +0100

    corrección

--- projects/ptfploe/actions/GetProjectAction.php
+++ projects/ptfploe/actions/GetProjectAction.php
@@ -4,14 +4,12 @@ if (!defined('DOKU_INC')) die();
 class GetProjectAction extends BasicGetUpdatableProjectAction {
 
     function runAction() {
-        $response = parent::runAction();
-
         $model = $this->getModel();
         
         if (! $model->isProjectGenerated()) {
             $model->setViewConfigName("firstView");
         }
-        return $response;
+        return parent::runAction();
     }
     
 }
