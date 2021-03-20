c226cf2179c3a69b008a968a8aca38a105e9f433
Josep <jcanell4@xtec.cat>
2021-03-20 19:39:14 +0100

    canvi d'ubicació dels responsehandlers locasl per tal de tenir el compte el tpl

--- metadata/templates/templateUpdateViewHandler.js
+++ metadata/templates/templateUpdateViewHandler.js
@@ -13,7 +13,8 @@ require([
             var id = disp.getGlobalState().getCurrentId();
             if (id) {
                 var page = disp.getGlobalState().getContent(id);
-                if (page && page.projectType === '%_projectType_%') {
+//                if (page && page.projectType === '%_projectType_%') {
+                if('%_conditionToProcess_%'){
                     //%_VarsIsButtonVisible_%
                     if (disp.getGlobalState().login) {
                         if (Object.keys(disp.getGlobalState().permissions).length > 0) {
