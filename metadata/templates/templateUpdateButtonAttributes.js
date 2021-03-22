ef862fa48e3458d67cdef78171b7adb066b9ed18
rclaver <rclaver@xtec.cat>
2021-03-22 11:21:55 +0100

    processCondition: page.workflowState === '%_workflowState_%'

--- metadata/templates/templateUpdateButtonAttributes.js
+++ metadata/templates/templateUpdateButtonAttributes.js
@@ -4,7 +4,7 @@ require([
      "dijit/registry",
     "dojo/domReady!"
     ], function (getDispatcher, UpdateViewHandler, registry) {
-        var oldValues={};
+        var oldValues = {};
         var wikiIocDispatcher = getDispatcher();
         var updateHandler = new UpdateViewHandler();
         updateHandler.update = function () {
@@ -13,50 +13,51 @@ require([
             if (id) {
                 var page = disp.getGlobalState().getContent(id);
 
-                if (page && page.projectType && page.projectType=='%_projectType_%') {
+                if (page && page.projectType === '%_projectType_%'  && (!page.workflowState || page.workflowState === '%_workflowState_%')) {
                     var buttons = ___JSON_BUTTON_ATTRIBUTES_DATA___;
                     buttons.forEach(function(buttonAttributes){
                         var buttonId = buttonAttributes.id;
                         var button = registry.byId(buttonId);
+                        var condition = page.projectType + (page.workflowState) ? page.workflowState : "";
 
-                        if(oldValues[page.projectType]==undefined || oldValues[page.projectType][buttonId]==undefined){
-                            if(oldValues[page.projectType]==undefined){
-                                oldValues[page.projectType]={};
+                        if (oldValues[condition] === undefined || oldValues[condition][buttonId] === undefined){
+                            if (oldValues[condition] === undefined){
+                                oldValues[condition] = {};
                             }
-                            oldValues[page.projectType][buttonId]={"toSet":{},"toDelete":[]};  
-                            if(buttonAttributes.toDelete && buttonAttributes.toDelete.length>0){
+                            oldValues[condition][buttonId] = {"toSet":{}, "toDelete":[]};
+                            if (buttonAttributes.toDelete && buttonAttributes.toDelete.length > 0) {
                                 buttonAttributes.toDelete.forEach(function(key){
-                                    if(button[key] !== undefined){
-                                        oldValues[page.projectType][buttonId]["toSet"][key] = button[key];
+                                    if (button[key] !== undefined){
+                                        oldValues[condition][buttonId]["toSet"][key] = button[key];
                                     }
                                 });
                             }
-                            if(buttonAttributes.toSet){
+                            if (buttonAttributes.toSet){
                                 for (const [key, value] of Object.entries(buttonAttributes.toSet)) {
-                                    if(button[key] == undefined){
-                                        oldValues[page.projectType][buttonId]["toDelete"]=key
+                                    if (button[key] === undefined){
+                                        oldValues[condition][buttonId]["toDelete"] = key
                                     }else{
-                                        oldValues[page.projectType][buttonId]["toSet"][key] = button[key];
+                                        oldValues[condition][buttonId]["toSet"][key] = button[key];
                                     }
                                 }
                             }
                         }
-                        if(buttonAttributes.toDelete && buttonAttributes.toDelete.length>0){
+                        if (buttonAttributes.toDelete && buttonAttributes.toDelete.length > 0) {
                             buttonAttributes.toDelete.forEach(function(key){
-                                if(button[key] !== undefined){
-                                    button[key]=undefined;
+                                if (button[key] !== undefined){
+                                    button[key] = undefined;
                                 }
                             });
                         }
-                        if(buttonAttributes.toSet){
+                        if (buttonAttributes.toSet){
                            for (const [key, value] of Object.entries(buttonAttributes.toSet)) {
                                button.set(key, value);
                            }
                         }
                     });
 
-                }else if(oldValues['%_projectType_%']!=undefined){
-                    for (const [buttonId, actions] of Object.entries(oldValues['%_projectType_%'])) {
+                }else if(oldValues['%_projectType_%%_workflowState_%'] !== undefined){
+                    for (const [buttonId, actions] of Object.entries(oldValues['%_projectType_%%_workflowState_%'])) {
                         var button = registry.byId(buttonId);
                         actions["toDelete"].forEach(function(element){
                             delete button[element];
