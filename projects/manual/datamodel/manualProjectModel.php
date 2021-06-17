4f14adb0314ffa2e399aeb78ee6761db4252d5f9
rclaver <rclaver@xtec.cat>
2021-06-17 16:20:35 +0200

    neteja

--- projects/manual/datamodel/manualProjectModel.php
+++ projects/manual/datamodel/manualProjectModel.php
@@ -9,20 +9,13 @@ class manualProjectModel extends AbstractProjectModel {
 
     public function __construct($persistenceEngine)  {
         parent::__construct($persistenceEngine);
-        $this->needGenerateAction=false;
+        $this->needGenerateAction = false;
     }
 
-    public function generateProject() {} //abstract obligatorio
+    public function generateProject(){} //abstract obligatorio
 
     public function directGenerateProject($data) {
-        //[TODO: Rafael] De momento, esto lo hace el padre durante la Creación
-        //2. Otorga, a las Persons, permisos sobre el directorio de proyecto y 3. añade enlace a dreceres
-//        $params = $this->buildParamsToPersons($data['projectMetaData'], NULL);
-//        $this->modifyACLPageAndShortcutToPerson($params);
-
-        //4. Establece la marca de 'proyecto generado'
         $ret = $this->projectMetaDataQuery->setProjectGenerated();
-
         return $ret;
     }
 
