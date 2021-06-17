d1ae4c06f19cccc095742362d41dd3de9b1f0684
rclaver <rclaver@xtec.cat>
2021-06-17 14:18:35 +0200

    control del botón FTP

--- projects/activityutil/actions/ViewProjectAction.php
+++ projects/activityutil/actions/ViewProjectAction.php
@@ -5,9 +5,7 @@ class ViewProjectAction extends BasicViewProjectAction{
 
     public function responseProcess() {
         $response = parent::responseProcess();
-        $projectModel = $this->getModel();
-        $response['generatedZipFiles'] = $projectModel->llistaDeEspaiDeNomsDeDocumentsDelProjecte();
-        $response[AjaxKeys::KEY_ACTIVA_FTP_PROJECT_BTN] = $projectModel->haveFilesToExportList();
+        $response['generatedZipFiles'] = $this->getModel()->llistaDeEspaiDeNomsDeDocumentsDelProjecte();
         return $response;
     }
 
