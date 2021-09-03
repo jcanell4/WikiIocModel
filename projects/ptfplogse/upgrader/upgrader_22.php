0507b6dacb2c21ef3385fd01d38646e3e6f16ee1
rclaver <rclaver@xtec.cat>
2021-09-03 09:08:00 +0200

    Control de la existencia de la PAF 2

---
+++ projects/ptfplogse/upgrader/upgrader_22.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * upgrader_22: Transforma el archivo continguts.tx del proyecto 'ptfplogse'
+ *              desde la versión 21 a la versión 22
+ * @author rafael <rclaver@xtec.cat>
+ */
+if (!defined("DOKU_INC")) die();
+if (!defined('DOKU_LIB_IOC')) define('DOKU_LIB_IOC', DOKU_INC."lib/lib_ioc/");
+require_once DOKU_LIB_IOC . "upgrader/CommonUpgrader.php";
+
+class upgrader_22 extends CommonUpgrader {
+
+    protected $model;
+    protected $metaDataSubSet;
+
+    public function __construct($model) {
+        $this->model = $model;
+        $this->metaDataSubSet = $this->model->getMetaDataSubSet();
+    }
+
+    public function process($type, $ver, $filename=NULL) {
+        switch ($type) {
+            case "fields":
+                $ret = TRUE;
+                break;
+
+            case "templates":
+                if ($filename===NULL) {
+                    $filename = $this->model->getProjectDocumentName();
+                }
+                $doc = $this->model->getRawProjectDocument($filename);
+
+                $aTokIns = [['regexp' => "^\|  1  \|  \{#_DATE\(\"\{##dataPaf11##\}\"\)_#\}.*\n",
+                             'text' => "<WIOCCL:IF condition=\"!{#_IS_STR_EMPTY(''{##dataPaf21##}'')_#}\">\n",
+                             'pos' => 1,
+                             'modif' => "m"],
+                            ['regexp' => "^\|  2  \|  \{#_DATE\(\"\{##dataPaf21##\}\"\)_#\}.*\n",
+                             'text' => "</WIOCCL:IF>\n",
+                             'pos' => 1,
+                             'modif' => "m"]];
+
+                $dataChanged = $this->updateTemplateByReplace($doc, $aTokIns);
+
+                if (($ret = !empty($dataChanged))) {
+                    $this->model->setRawProjectDocument($filename, $dataChanged, "Upgrade templates: version ".($ver-1)." to $ver", $ver);
+                }
+                break;
+        }
+        return $ret;
+    }
+
+}
