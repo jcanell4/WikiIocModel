ddabeda4ad17eb0267206837c49bae09e087a0fa
rclaver <rclaver@xtec.cat>
2021-11-03 10:03:05 +0100

    upgrader_7 - prgloe, upgrader_4 - prglogse

---
+++ projects/prgfploe/upgrader/upgrader_7.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * upgrader_7: Transforma el archivo continguts.txt de los proyectos 'prgfploe'
+ *             desde la versión 6 a la versión 7
+ * @author rafael <rclaver@xtec.cat>
+*/
+if (!defined("DOKU_INC")) die();
+if (!defined('DOKU_LIB_IOC')) define('DOKU_LIB_IOC', DOKU_INC."lib/lib_ioc/");
+require_once DOKU_LIB_IOC . "upgrader/CommonUpgrader.php";
+
+class upgrader_7 extends CommonUpgrader {
+
+    public function process($type, $ver, $filename=NULL) {
+        switch ($type) {
+            case "fields":
+                //Transforma los datos del proyecto desde la estructura de la versión $ver a la versión $ver+1
+                $ret = true;
+                break;
+            case "templates":
+                // Actualiza la versión del documento establecido en el sistema de calidad del IOC (Visible en el pie del documento)
+                // Sólo se debe actualizar si el coordinador de calidad lo indica!!!!!!
+                /*
+                $dataProject = $this->model->getCurrentDataProject($this->metaDataSubSet);
+                if (!is_array($dataProject))
+                    $dataProject = json_decode($dataProject, TRUE);
+                $dataProject['documentVersion'] = $dataProject['documentVersion']+1;
+                $ret = $this->model->setDataProject(json_encode($dataProject), "Upgrade fields: version ".($ver-1)." to $ver", '{"fields":'.$ver.'}');
+                 */
+
+                //Transforma el archivo continguts.txt del proyecto desde la versión $ver a la versión $ver+1
+                if ($filename===NULL)
+                    $filename = $this->model->getProjectDocumentName();
+                $doc = $this->model->getRawProjectDocument($filename);
+
+                $aTokRep = [['(\{##taulaDadesBlocs##\}\)_#\}.>)(0)',
+                             '${1}1']
+                           ];
+                $dataChanged = $this->updateTemplateByReplace($doc, $aTokRep);
+
+                if (($ret = !empty($dataChanged))) {
+                    $this->model->setRawProjectDocument($filename, $dataChanged, "Upgrade templates: version ".($ver-1)." to $ver", $ver);
+                }
+                break;
+        }
+        return $ret;
+    }
+
+}
