3a1c28b66f36e1f7cc98d76f9d386680ed1cea28
rclaver <rclaver@xtec.cat>
2021-12-13 11:26:06 +0100

    correcció

--- projects/manual/datamodel/manualProjectModel.php
+++ projects/manual/datamodel/manualProjectModel.php
@@ -25,32 +25,34 @@ class manualProjectModel extends AbstractProjectModel {
      */
     public function validateFields($data=NULL){
         if ($data) {
-            $nousDocuments = json_decode($data['documents'], true);
+            $id = $this->getId();
+            $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $id);
+
+            $nousDocuments = is_array($data['documents']) ? $data['documents'] : json_decode($data['documents'], true);
             if (!empty($nousDocuments)) {
                 usort($nousDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
 
                 $dataProject = $this->getCurrentDataProject();
-                $vellsDocuments = json_decode($dataProject['documents'], true);
+                $vellsDocuments = is_array($dataProject['documents']) ? $dataProject['documents'] : json_decode($dataProject['documents'], true);
                 usort($vellsDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
 
-                $id = $this->getId();
-                $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $id);
-
                 foreach ($nousDocuments as $k => $doc) {
-                    if ($doc['id'] === $vellsDocuments[$k]['id']) {
-                        //S'ha modificat el nom d'un fitxer
+                    if ($doc['id'] == $vellsDocuments[$k]['id']) {
+                        //comprovem si s'ha modificat el nom d'un fitxer
                         if ($doc['nom'] !== $vellsDocuments[$k]['nom']) {
                             $this->renamePage($id, $path_continguts, $vellsDocuments[$k]['nom'], $doc['nom']);
                         }
                     }elseif ($doc['id'] > $vellsDocuments[$k]['id'] && $vellsDocuments[$k]) {
-                        $rowid = array_search($doc['id'], array_column($vellsDocuments, 'id'));
                         // busca el id actual en todo el array de $vellsDocuments
+                        $rowid = array_search($doc['id'], array_column($vellsDocuments, 'id'));
                         if ($rowid !== false && $doc['nom'] !== $vellsDocuments[$rowid]['nom']) {
                             $this->renamePage($id, $path_continguts, $vellsDocuments[$rowid]['nom'], $doc['nom']);
                         }
                     }else {
                         //S'ha afegit un nou fitxer, és a dir, una nova fila a la taula
-                        $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
+                        if (!file_exists("$path_continguts/{$doc['nom']}.txt")) {
+                            $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
+                        }
                     }
                 }
             }
@@ -77,7 +79,7 @@ class manualProjectModel extends AbstractProjectModel {
      * Callback de usort. Retorna el valor de la comparación de 2 elementos. Se comparan los valores de las claves 'id'
      * @param array $a
      * @param array $b
-     * @return int : 0 $a==$b; 1 $a < $b; -1 $a < $b
+     * @return int : 0 $a==$b; -1 $a < $b; 1 $b <= $a
      */
     static function cmpForSort($a, $b) {
         return ($a['id'] === $b['id']) ? 0 : (($a['id'] < $b['id']) ? -1 : 1);
