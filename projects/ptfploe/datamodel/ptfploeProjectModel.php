6035949a6d31dfbc7519c8a46c852be27cd849a9
jcanell4 <jcanell4@xtec.cat>
2022-05-25 12:38:12 +0200

    Millores en els plans de treball passant a la versió 35 (loe) i 29 (LOGSE)

--- projects/ptfploe/datamodel/ptfploeProjectModel.php
+++ projects/ptfploe/datamodel/ptfploeProjectModel.php
@@ -305,6 +305,7 @@ class ptfploeProjectModel extends MoodleUniqueContentFilesProjectModel {
                     $calLen = count($aux);
                     $modulId = trim(values["modulId"]);
                     if(preg_match("/$modulId/i", $dataFromMix[0]->shortname)){ 
+//                        error_log("D0.3.- A punt d'actualitzar.");
                         $aux = $taulaCalendari;
                         $taulaCalendari = array();
                         for($i=0; $i<$mixLen; $i++){                   
