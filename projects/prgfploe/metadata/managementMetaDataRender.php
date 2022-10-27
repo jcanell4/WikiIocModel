b379a074696215527510a550237e8014f0c5d530
rclaver <rclaver@xtec.cat>
2022-10-27 09:42:48 +0200

    abstracció de class MetaDataRender

---
+++ projects/prgfploe/metadata/managementMetaDataRender.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * class managementMetaDataRender: render del subset 'management' de 'prgfploe'
+*/
+if (!defined("DOKU_INC")) die();
+
+class managementMetaDataRender extends BasicMetaDataRender {}
