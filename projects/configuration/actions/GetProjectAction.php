af799bd414c1909c8d6084e3b2f901c1f893ffe4
Xavier Garcia <xaviergaro.dev@gmail.com>
2023-02-25 11:56:47 +0100

    Solcucionat problema de càrrega de vistes

---
+++ projects/configuration/actions/GetProjectAction.php
@@ -0,0 +1,42 @@
+<?php
+if (!defined('DOKU_INC')) die();
+
+class GetProjectAction extends BasicGetProjectAction{
+
+    protected $view;
+
+    protected function preResponseProcess() {
+        parent::preResponseProcess();
+
+        $userInfo = WikiIocInfoManager::getInfo("userinfo");
+        $user_groups = $userInfo['grps'];
+
+        $model = $this->getModel();
+        $action = $model->getMetaDataActionViews();
+
+        $view = $action["default"];
+            if (($views_group = $action['groups'])) {
+                foreach ($views_group as $g => $vista) {
+                    if (in_array($g, $user_groups)) {
+                        $view = $vista;
+                        break;
+                    }
+                }
+            }
+
+        $file = $model->getProjectMetaDataQuery()->getProjectTypeDir()."metadata/config/{$view}.json";
+        if ($view && is_file($file)) {
+            $this->view = $view;
+        }
+    }
+
+    protected function runAction() {
+
+        if($this->getModel()->getViewConfigKey()===ProjectKeys::KEY_VIEW_DEFAULTVIEW && $this->view){
+            $this->getModel()->setViewConfigKey($this->view);
+        }
+        $response = parent::runAction();
+        return $response;
+    }
+
+}
