68671adfe059c758044a6d1c336c891127e9330c
jcanell4 <jcanell4@xtec.cat>
2023-09-04 23:13:13 +0200

    fix delete, create or rename documents from array document

--- projects/manual/datamodel/manualProjectModel.php
+++ projects/manual/datamodel/manualProjectModel.php
@@ -27,36 +27,51 @@ class manualProjectModel extends AbstractProjectModel {
         if($subset!==FALSE && $subset!=ProjectKeys::VAL_DEFAULTSUBSET){
             return parent::validateFields($data, $subset);
         }
-
+        
         if ($data) {
-            $id = $this->getId();
-            $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $id);
-
-            $nousDocuments = is_array($data['documents']) ? $data['documents'] : json_decode($data['documents'], true);
+            $modificat = false;
+            $nousDocuments = json_decode($data['documents'], true);
             if (!empty($nousDocuments)) {
+                foreach ($nousDocuments as $k => $doc) {
+                    $nousDocuments[$k]['nom'] =  str_replace(" ", "_", trim($doc['nom']));
+                }
                 usort($nousDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
 
                 $dataProject = $this->getCurrentDataProject();
-                $vellsDocuments = is_array($dataProject['documents']) ? $dataProject['documents'] : json_decode($dataProject['documents'], true);
-                usort($vellsDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
+                $vellsDocuments = json_decode($dataProject['documents'], true);
+                if (!empty($vellsDocuments)) {
+                    foreach ($vellsDocuments as $k => $doc) {
+                        $vellsDocuments[$k]['nom'] = str_replace(" ", "_", trim($doc['nom']));
+                    }
+                    usort($vellsDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
+                }
+
+                $id = $this->getId();
+                $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $id);
 
                 foreach ($nousDocuments as $k => $doc) {
-                    if ($doc['id'] == $vellsDocuments[$k]['id']) {
-                        //comprovem si s'ha modificat el nom d'un fitxer
+                    if ($doc['id'] === $vellsDocuments[$k]['id']) {
                         if ($doc['nom'] !== $vellsDocuments[$k]['nom']) {
+                            //S'ha modificat el nom d'un fitxer
+                            $modificat = true;
                             $this->renamePage($id, $path_continguts, $vellsDocuments[$k]['nom'], $doc['nom']);
                         }
                     }elseif ($doc['id'] > $vellsDocuments[$k]['id'] && $vellsDocuments[$k]) {
-                        // busca el id actual en todo el array de $vellsDocuments
                         $rowid = array_search($doc['id'], array_column($vellsDocuments, 'id'));
-                        if ($rowid !== false && $doc['nom'] !== $vellsDocuments[$rowid]['nom']) {
+                        $rownom = array_search($doc['nom'], array_column($vellsDocuments, 'nom'));
+                        if ($rowid === false && $rownom === false) {
+                            //S'ha afegit un nou fitxer, és a dir, una nova fila a la taula
+                            $modificat = true;
+                            $this->createPageFromTemplate("$id:${doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
+                        }elseif ($rowid !== false && $doc['nom'] !== $vellsDocuments[$rowid]['nom']) {
+                            //S'ha modificat el nom d'un fitxer
+                            $modificat = true;
                             $this->renamePage($id, $path_continguts, $vellsDocuments[$rowid]['nom'], $doc['nom']);
                         }
                     }else {
                         //S'ha afegit un nou fitxer, és a dir, una nova fila a la taula
-                        if (!file_exists("$path_continguts/{$doc['nom']}.txt")) {
-                            $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
-                        }
+                        $modificat = true;
+                        $this->createPageFromTemplate("$id:${doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
                     }
                 }
             }
@@ -69,14 +84,75 @@ class manualProjectModel extends AbstractProjectModel {
                         $rownom = array_search($doc['nom'], array_column($nousDocuments, 'nom'));
                     }
                     if ($rowid === false && $rownom === false) {
-                        $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, NULL, "remove page");
+                        $modificat = true;
+                        $this->createPageFromTemplate("$id:${doc['nom']}", NULL, NULL, "remove page");
                     }
                 }
+            }
+
+            if ($modificat) {
+                //Actualización de _wikiIocSystem_.mdpr
+                $dataSystem = $this->getSystemData();
+                $dataSystem['versions']['templates'] = [];
+                foreach ($nousDocuments as $doc) {
+                    $dataSystem['versions']['templates'][$doc['nom']] = null;
+                }
+                $this->setSystemData($dataSystem);
             }
         }
         else {
             throw new Exception("Aquí passa alguna cosa rara");
         }
+
+//        if ($data) {
+//            $id = $this->getId();
+//            $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $id);
+//
+//            $nousDocuments = is_array($data['documents']) ? $data['documents'] : json_decode($data['documents'], true);
+//            if (!empty($nousDocuments)) {
+//                usort($nousDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
+//
+//                $dataProject = $this->getCurrentDataProject();
+//                $vellsDocuments = is_array($dataProject['documents']) ? $dataProject['documents'] : json_decode($dataProject['documents'], true);
+//                usort($vellsDocuments, 'self::cmpForSort');  //ordenamos el array por el campo 'id'
+//
+//                foreach ($nousDocuments as $k => $doc) {
+//                    if ($doc['id'] == $vellsDocuments[$k]['id']) {
+//                        //comprovem si s'ha modificat el nom d'un fitxer
+//                        if ($doc['nom'] !== $vellsDocuments[$k]['nom']) {
+//                            $this->renamePage($id, $path_continguts, $vellsDocuments[$k]['nom'], $doc['nom']);
+//                        }
+//                    }elseif ($doc['id'] > $vellsDocuments[$k]['id'] && $vellsDocuments[$k]) {
+//                        // busca el id actual en todo el array de $vellsDocuments
+//                        $rowid = array_search($doc['id'], array_column($vellsDocuments, 'id'));
+//                        if ($rowid !== false && $doc['nom'] !== $vellsDocuments[$rowid]['nom']) {
+//                            $this->renamePage($id, $path_continguts, $vellsDocuments[$rowid]['nom'], $doc['nom']);
+//                        }
+//                    }else {
+//                        //S'ha afegit un nou fitxer, és a dir, una nova fila a la taula
+//                        if (!file_exists("$path_continguts/{$doc['nom']}.txt")) {
+//                            $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, $this->getRawProjectTemplate(), "create page");
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (!empty($vellsDocuments)) {
+//                //En el cas que s'hagin eliminat files de la taula de documents
+//                foreach ($vellsDocuments as $doc) {
+//                    if (!empty($nousDocuments)) {
+//                        $rowid = array_search($doc['id'], array_column($nousDocuments, 'id'));
+//                        $rownom = array_search($doc['nom'], array_column($nousDocuments, 'nom'));
+//                    }
+//                    if ($rowid === false && $rownom === false) {
+//                        $this->createPageFromTemplate("$id:{$doc['nom']}", NULL, NULL, "remove page");
+//                    }
+//                }
+//            }
+//        }
+//        else {
+//            throw new Exception("Aquí passa alguna cosa rara");
+//        }
     }
 
     /**
