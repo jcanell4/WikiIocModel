e10907b10d4555517676a9ef9cabee748d94ced2
professor <marjose@ioc.cat>
2024-09-17 12:07:52 +0200

    New upgrader_v1 for ptce

--- projects/ptce/upgrader/upgrader_1.php
+++ projects/ptce/upgrader/upgrader_1.php
@@ -14,15 +14,20 @@ class upgrader_1 extends CommonUpgrader {
     public function process($type, $ver, $filename=NULL) {
         switch ($type) {
             case "fields":
+                //Transforma los datos del proyecto desde la estructura de la versión $ver a la versión $ver+1
+                $ret = true;
+                break;
+            
                 //Transforma los datos del proyecto "ptce" desde la estructura de la versión 0 a la versión 1
-                $dataProject = $this->model->getCurrentDataProject($this->metaDataSubSet);
+                /*$dataProject = $this->model->getCurrentDataProject($this->metaDataSubSet);
                 if (!is_array($dataProject)) {
                     $dataProject = json_decode($dataProject, TRUE);
-                }
+                }*/
                 //Añade el campo 'hiHaRecuperacio' a la tabla 'datesJT'
                 //$dataProject = $this->addFieldInMultiRow($dataProject, "datesJT", "hiHaRecuperacio", TRUE);
                 
                 //Cambia el nombre del campo
+                /*
                 $dataProject = $this->changeFieldName($dataProject, "dataPaf11", "dataPv1");
                 $dataProject = $this->changeFieldName($dataProject, "dataPaf12", "dataPv2");
                 $dataProject = $this->changeFieldName($dataProject, "dataPaf21", "dataPaf1");
@@ -33,7 +38,7 @@ class upgrader_1 extends CommonUpgrader {
                 $ret = $this->model->setDataProject(json_encode($dataProject), "Upgrade fields: version ".($ver-1)." to $ver", '{"fields":'.$ver.'}');
                 break;
                 
-                
+                */
                 
                 
                 
@@ -57,43 +62,30 @@ class upgrader_1 extends CommonUpgrader {
                  */
 
             case "templates":
-                // Línia 96.  Es canvia "  :title:Taula Unitats" per "  :title:Apartats"
-                // Línia 102. Es canvia "{#_DATE("{##itemc[inici]##}", ".")_#}-{#_DATE("{##itemc[inici]##}" per "{#_DATE("{##itemc[inici]##}", ".")_#}-{#_DATE("{##itemc[final]##}"
-
-                
-                //Línia 275. Es canvia "|  1  |  {#_DATE("{##dataPaf11##}")_#}<WIOCCL:IF condition="!{#_IS_STR_EMPTY(''{##dataPaf12##}'')_#}"> o {#_DATE("{##dataPaf12##}")_#}</WIOCCL:IF>  |  {#_DATE("{##dataQualificacioPaf1##}")_#}  |" per "|  PV  |  {#_DATE("{##dataPv1##}")_#}<WIOCCL:IF condition="!{#_IS_STR_EMPTY(''{##dataPv2##}'')_#}"> o {#_DATE("{##dataPv2##}")_#}</WIOCCL:IF>  |  {#_DATE("{##dataQualificacioPv##}")_#}  |"
-                //Línia 276. Es canvia "|  2  |  {#_DATE("{##dataPaf21##}")_#}<WIOCCL:IF condition="!{#_IS_STR_EMPTY(''{##dataPaf22##}'')_#}"> o {#_DATE("{##dataPaf22##}")_#}</WIOCCL:IF>  |  {#_DATE("{##dataQualificacioPaf2##}")_#}  |" per "|  PAF  |  {#_DATE("{##dataPaf1##}")_#}<WIOCCL:IF condition="!{#_IS_STR_EMPTY(''{##dataPaf2##}'')_#}"> o {#_DATE("{##dataPaf2##}")_#}</WIOCCL:IF>  |  {#_DATE("{##dataQualificacioPaf##}")_#}  |"
-                
-                if ($filename===NULL) { //Ojo! Ahora se pasa por parámetro
+     
+                //Transforma el archivo continguts.txt del proyecto desde la versión $ver a la versión $ver+1
+                if ($filename===NULL) { 
                     $filename = $this->model->getProjectDocumentName();
                 }
                 $doc = $this->model->getRawProjectDocument($filename);
 
-                /*
-                $aTokRep = [["dataPaf11","dataPv1"],
-                ["dataPaf12","dataPv2"],
-                ["|  1  |  {#_DATE\(","|  PV  |  \{#_DATE\("],
-                ["dataPaf21","dataPaf1"],
-                ["dataPaf22","dataPaf2"],
-                ["|  2  |  {#_DATE\(","|  PAF  |  \{#_DATE\("]];
-                 * */
+                //Línies 57-59. Es canviaen per Línies 57-59.
                 
-                /*
-                $aTokRep = [[" +:title:Taula Unitats",
-                             "  :title:Apartats"],
-                            ["{#_DATE\(\"{##itemc\[inici\]##}\", \"\.\"\)_#}-{#_DATE\(\"{##itemc\[inici\]##}",
-                             "{#_DATE(\"{##itemc[inici]##}\", \".\")_#}-{#_DATE(\"{##itemc[final]##}"]];
-                $dataChanged = $this->updateTemplateByReplace($doc, $aTokRep);
-                */
-                
-                
+                $txt_v0 = $this->model->getRawProjectTemplate("continguts", 0);
+                $txt_v1 = $this->model->getRawProjectTemplate("continguts", 1);
+                $l57_l59_v0 =  $this->substringFromLineToLineAsPattern($txt_v0, 57, 59);
+                $l57_l59_v1 = $this->substringFromLineToLine($txt_v1,57, 59);
+                $l74_l136_v0 =  $this->substringFromLineToLineAsPattern($txt_v0, 74, 136);//el que busca
+                $l74_l103_v1 = $this->substringFromLineToLine($txt_v1, 74, 103);//on ho substitueix
+                $l336_l415_v0 =  $this->substringFromLineToLineAsPattern($txt_v0, 336, 415);
+                $l303_l305_v1 = $this->substringFromLineToLine($txt_v1, 303, 305);    
+                $l431_l468_v0 =  $this->substringFromLineToLineAsPattern($txt_v0, 431, 468);
+                $l321_l359_v1 = $this->substringFromLineToLine($txt_v1, 321, 359); 
                 $aTokRep = [
-                    ["  1  \\| \\< \\{#_DATE\\(\"\\{##dataPaf11##\\}\")_#\\}\\<WIOCCL:IF condition\\=\"!\\{#_IS_STR_EMPTY\\(''\\{##dataPaf12##\\}'')_#\\}\"\\> o \\{#_DATE\\(\"\\{##dataPaf12##\\}\")_#\\}\\</WIOCCL:IF\\>  \\|  \\{#_DATE\\(\"\\{##dataQualificacioPaf1##\\}\")_#\\}  \\|" , 
-                     "  PV  \\| \\< \\{#_DATE\\(\"\\{##dataPv1##\\}\")_#\\}\\<WIOCCL:IF condition\\=\"!\\{#_IS_STR_EMPTY\\(''\\{##dataPv2##\\}'')_#\\}\"\\> o \\{#_DATE\\(\"\\{##dataPv2##\\}\")_#\\}\\</WIOCCL:IF\\>  \\|  \\{#_DATE\\(\"\\{##dataQualificacioPv##\\}\")_#\\}  \\|"
-                    ],
-                    ["  2  \\| \\< \\{#_DATE\\(\"\\{##dataPaf21##\\}\")_#\\}\\<WIOCCL:IF condition\\=\"!\\{#_IS_STR_EMPTY\\(''\\{##dataPaf22##\\}'')_#\\}\"\\> o \\{#_DATE\\(\"\\{##dataPaf22##\\}\")_#\\}\\</WIOCCL:IF\\>  \\|  \\{#_DATE\\(\"\\{##dataQualificacioPaf2##\\}\")_#\\}  \\|" , 
-                     "  PAF  \\| \\< \\{#_DATE\\(\"\\{##dataPaf##\\}\")_#\\}\\<WIOCCL:IF condition\\=\"!\\{#_IS_STR_EMPTY\\(''\\{##dataPaf##\\}'')_#\\}\"\\> o \\{#_DATE\\(\"\\{##dataPaf##\\}\")_#\\}\\</WIOCCL:IF\\>  \\|  \\{#_DATE\\(\"\\{##dataQualificacioPaf##\\}\")_#\\}  \\|"
-                    ]                    
+                    [$l57_l59_v0, $l57_l59_v1],
+                    [$l74_l136_v0, $l74_l103_v1],
+                    [$l336_l415_v0, $l303_l305_v1],
+                    [$l431_l468_v0, $l321_l359_v1]
                 ];
                 $dataChanged = $this->updateTemplateByReplace($doc, $aTokRep);
 
