e8d6724a266e7086a34128084e67603cee14efd1
professor <marjose@ioc.cat>
2025-01-27 17:25:48 +0100

    New guieseoi anual icones New upgrader for guieseoi

--- projects/guieseoi/upgrader/upgrader_3.php
+++ projects/guieseoi/upgrader/upgrader_3.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * upgrader_2: Transforma la estructura de datos y el archivo continguts.txt de los proyectos 'guieseoi'
- *             desde la versión 1 a la versión 2
+ * upgrader_3: Transforma la estructura de datos y el archivo continguts.txt de los proyectos 'guieseoi'
+ *             desde la versión 2 a la versión 3
  * @author rafael
  * @adapter marjose
  */
@@ -9,7 +9,7 @@ if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_LIB_IOC')) define('DOKU_LIB_IOC', DOKU_INC."lib/lib_ioc/");
 require_once DOKU_LIB_IOC . "upgrader/CommonUpgrader.php";
 
-class upgrader_2 extends CommonUpgrader {
+class upgrader_3 extends CommonUpgrader {
 
     public function process($type, $ver, $filename=NULL) {
         switch ($type) {
@@ -28,13 +28,13 @@ class upgrader_2 extends CommonUpgrader {
                 $doc = $this->model->getRawProjectDocument($filename);
                 
                 
-                $txt_v1 = $this->model->getRawProjectTemplate("continguts", 1);
                 $txt_v2 = $this->model->getRawProjectTemplate("continguts", 2);
-                $l49_l64_v1 =  $this->substringFromLineToLineAsPattern($txt_v1, 49, 64);
-                $l49_l49_v2 = $this->substringFromLineToLine($txt_v2,49, 49);
+                $txt_v3 = $this->model->getRawProjectTemplate("continguts", 3);
+                $l20_l22_v2 =  $this->substringFromLineToLineAsPattern($txt_v2, 20, 22);
+                $l68_l70_v3 = $this->substringFromLineToLine($txt_v3,68, 70);
    
                 $aTokRep = [
-                    [$l49_l64_v1, $l49_l49_v2]
+                    [$l20_l22_v2, $l68_l70_v3]
                 ];
                 $dataChanged = $this->updateTemplateByReplace($doc, $aTokRep);
 
